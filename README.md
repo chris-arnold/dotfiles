d5ccc25556aa97a7d22078269561c7374dfc0541
Chris Arnold <chris.arnold@leadpages.net>
2018-11-27 16:49:29 -0600

    Updates for transfer

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # dotfiles
 
+## Fonts
+
+Provided in zip, sources below.
+
+[PTMono](https://github.com/google/fonts/blob/master/ofl/ptmono/PTM55FT.ttf)
+[Anonymice Powerline](https://github.com/powerline/fonts/tree/master/AnonymousPro)
+[Ubuntu Mono](https://github.com/google/fonts/tree/master/ufl/ubuntumono)
+
+Also see [ProgrammingFonts](https://github.com/ProgrammingFonts/ProgrammingFonts)
+
+
 ## Installing
 
 ## Other Notes
---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#
+# Intended to be cloned and run, not sourced from github
+#
+# This assumes zsh is already installed.
+
+# Clone location for this repo - zshrc is symlinked to ~/.zshrc
+ZSH_DOTFILES=$HOME/dotfiles/zsh
+
+# Install location for Fuzzy Finder.
+FZF_DOTFILES=$HOME/.fzf
+
+# Clone location for ZSH plugins.
+ZSH_INCLUDES=$HOME/.local/share/zsh_includes
+
+GITCLONE="git clone --depth=1"
+
+# Start from scratch.
+rm -rf \
+  $FZF_DOTFILES
+  $ZSH_INCLUDES \
+  $HOME/.zshrc
+
+ln -s $ZSH_DOTFILES/zshrc $HOME/.zshrc
+
+# Install FZF with key bindings and completion.
+$GITCLONE https://github.com/junegunn/fzf.git $FZF_DOTFILES
+$DOTFZF/install --key-bindings --completion --no-update-rc
+
+# Clone any ZSH plugins.
+mkdir -p $ZSH_INCLUDES
+$GITCLONE https://github.com/zsh-users/zsh-completions.git $ZSH_INCLUDES/zsh-completions
+$GITCLONE https://github.com/zsh-users/zsh-history-substring-search.git $ZSH_INCLUDES/zsh-history-substring-search
+$GITCLONE https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_INCLUDES/zsh-syntax-highlighting
+$GITCLONE https://github.com/rupa/z.git $ZSH_INCLUDES/z
+if [ ! -f "$HOME/.z" ]; then
+  touch $HOME/.z
+fi
+
+cd $HOME
+rm -f $HOME/.zcompdump*
+
+# NVM
+if [ -d "$HOME/.nvm" ]; then
+  $GITCLONE https://github.com/creationix/nvm.git $HOME/.nvm
+fi
+
+# GVM - Disabled for now.
+# if ! command -v gvm; then
+#   zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+# fi
+
